45b1b81d177a659a7f4f0e07dba8594105172801
prasadravi <prasadravi9866@gmail.com>
2026-04-11 22:00:56 +0530

    your message

---
+++ reverse.java
@@ -0,0 +1,13 @@
+public class reverse {
+    public static void main(String[] args){
+        String str = "Hello";
+        String rev="";
+        for(int i=str.length()-1;i>=0;i--){
+            rev+=str.charAt(i);
+
+
+        }
+        System.out.println("Reverse of the string is: " + rev);
+     }
+    
+}
