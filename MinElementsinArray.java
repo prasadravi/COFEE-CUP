9d7ebeb9f59e7b39dce4a86f826e68264ab8cff9
prasadravi <prasadravi9866@gmail.com>
2026-04-21 06:35:38 +0530

    DSA

---
+++ MinElementsinArray.java
@@ -0,0 +1,13 @@
+public class MinElementsinArray {
+    public static void main(String[] args){
+        int[] arr={3,7,2,9,5};
+        int min=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println("Min Element is: " + min);
+    }
+    
+}
